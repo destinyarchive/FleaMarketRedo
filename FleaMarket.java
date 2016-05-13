2171f16f6052cb208b53a911cfe4890287069c82
Will <Destinyarchive@outlook.com>
2016-05-13 10:26:42 -0400

    fixed some errors. Needs functionality

--- FleaMarket.java
+++ FleaMarket.java
@@ -3,37 +3,18 @@ class FleaMarket{
   public List<Item> itemList;
   private List<Item> itemSum;
   private double orderTotal;
-  public void itemPurchase(){
-    String name = getName();
-    double price = getPrice();
-    int quant = getQuant();
-    itemList.add(new Item(name, price, quant));
-    orderTotal += (price*quant);
+
+  public FleaMarket(){//initializes ArrayLists itemList and itemSum
+    itemList = new ArrayList<Item>();
+    itemSum = new ArrayList<Item>();
   }
-  public String getName(){
-    Scanner userIn = new Scanner(System.in);
-    System.out.println("ENTER ITEM NAME");
-    String userName = userIn.nextLine();
-    return userName;
-  }
-  public double getPrice(){
-    Scanner userIn = new Scanner(System.in);
-    System.out.println("ENTER ITEM PRICE");
-    double userPrice = (double)(userIn.nextInt());
-    return userPrice;
-  }
-  public int getQuant(){
-    Scanner userIn = new Scanner(System.in);
-    System.out.println("ENTER ITEM QUANTITY");
-    int userQuant = userIn.nextInt();
-    return userQuant;
-  }
-  public void clearList(){
+
+  public void clearList(){//clears itemList and adds all contents to itemSum
     while (itemList.size() > 0){
       itemSum.add(itemList.remove(0));
     }
   }
-  public void order(){
+  public void order(){//Runs one order
     String cont = "Y";
     while ((cont.toUpperCase()).equals("Y")){
       itemPurchase();
@@ -41,11 +22,51 @@ class FleaMarket{
       System.out.println("CONTINUE? (please answer Y or N)");
       cont = userIn.nextLine();
     }
+
+    //Prints out a receipt
     for (int c = 0; c < itemList.size(); c++){
       Item temp = itemList.get(c);
-      temp.toString();
+      System.out.println(temp.toString());
     }
     System.out.print("                             Order Total: $" + orderTotal);
     clearList();
+  }
+
+  public void itemPurchase(){//creates a new item in itemList and adds to orderTotal
+    String name = getName();
+    double price = getPrice();
+    int quant = getQuant();
+    itemList.add(new Item(name, price, quant));
+    orderTotal += (price*quant);
+  }
+
+  public String daysOrders(){//printout for end of day
+    String result = "";
+    int totalSumItems = 0;
+    for (int x = 0; x < itemSum.size(); x++){
+      result += (itemSum.get(x)).getItemName() + " - $" + (itemSum.get(x)).getItemPrice() + "\n";
+      totalSumItems += (itemSum.get(x)).getItemPrice();
+    }
+    result += "                                  Total Sum: $" + totalSumItems;
+    return result;
+  }
+
+  public String getName(){//collects user data
+    Scanner userIn = new Scanner(System.in);
+    System.out.println("ENTER ITEM NAME");
+    String userName = userIn.nextLine();
+    return userName;
+  }
+  public double getPrice(){//collects user data
+    Scanner userIn = new Scanner(System.in);
+    System.out.println("ENTER ITEM PRICE");
+    double userPrice = Double.parseDouble(userIn.nextLine());
+    return userPrice;
+  }
+  public int getQuant(){//collects user data
+    Scanner userIn = new Scanner(System.in);
+    System.out.println("ENTER ITEM QUANTITY");
+    int userQuant = Integer.parseInt(userIn.nextLine());
+    return userQuant;
   }
 }
