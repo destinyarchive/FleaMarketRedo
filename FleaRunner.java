6258a5995722f948f37b904d5a842bc3e6e2ffee
Will <Destinyarchive@outlook.com>
2016-05-13 10:36:11 -0400

    Added functionality

--- FleaRunner.java
+++ FleaRunner.java
@@ -1,6 +1,14 @@
+import java.util.*;
 class FleaRunner{
   public static void main(String[] args){
+    String dayCont = "Y";
     FleaMarket myMarket = new FleaMarket();
-    myMarket.order();
+    while (dayCont.equals("Y")){
+      myMarket.order();
+      Scanner user = new Scanner(System.in);
+      System.out.println("ANOTHER ORDER? (ANSWER Y OR N)\n");
+      dayCont = user.nextLine();
+    }
+    myMarket.daysOrders();
   }
 }
