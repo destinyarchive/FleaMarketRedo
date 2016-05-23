a5f45b622ed24dc958165d0607a6d2597dd8550b
Will <Destinyarchive@outlook.com>
2016-05-23 10:38:45 -0400

    Changed item Name to Booth Number

--- test/ItemTest.java
+++ test/ItemTest.java
@@ -5,14 +5,14 @@ public class ItemTest {
 
   @Test
   public void addsTax() {
-    Item subject = new Item("Banana", 0.49, 10);
+    Item subject = new Item("1", 0.49, 10);
     assertEquals(subject.addTax(10.0, 0.5), 15.0, 0.00001);
   }
 
   @Test
   public void hasVars(){
-    Item subject = new Item("orange", .25, 2);
-    assertEquals(subject.getItemName(), "orange");
+    Item subject = new Item("1", .25, 2);
+    assertEquals(subject.getBoothNumber(), "1");
     assertEquals(subject.getItemPrice(), 0.25, 0.00001);
     assertEquals(subject.getItemQuantity(), 2);
     assertEquals(subject.getItemTaxedPrice(), .25*1.06, 0.00001);
@@ -20,7 +20,7 @@ public class ItemTest {
 
   @Test
   public void itemToString(){
-    Item subject = new Item("orange", .25, 2);
-    assertEquals(subject.toString(), "orange  -  2  -  $0.25");
+    Item subject = new Item("1", .25, 2);
+    assertEquals(subject.toString(), "1  -  2  -  $0.25");
   }
 }
